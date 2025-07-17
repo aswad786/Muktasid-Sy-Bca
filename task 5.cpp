bcb4eca329a21766af173999b981ed7a15c2bb66
aswad786 <sayyedaswad43@gmail.com>
2025-07-18 00:08:51 +0530

    Add files via upload

---
+++ task 5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	cout<<"Size of Integer:"<<sizeof (int)<<endl;
+	cout<<"Size of Float:"<<sizeof (float)<<endl;
+    cout<<"Size of Double:"<<sizeof (double)<<endl;
+    cout<<"Size of Character:"<<sizeof (char)<<endl;
+    cout<<"Size of bool:"<<sizeof (bool)<<endl;
+	
+	return 0;
+}
