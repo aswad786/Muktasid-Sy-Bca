2a085cd0fe316b3b157d55a88a5adb742e6cd941
aswad786 <sayyedaswad43@gmail.com>
2025-07-18 11:34:54 +0530

    Add files via upload

---
+++ calculator .h
@@ -0,0 +1,42 @@
+#include<iostream>
+    using namespace std;
+    
+    inline void show (){
+    	double a,b;
+    	char op;
+    	
+    	cout<<"Enter First Number:";
+    	    cin>>a;
+        cout<<"Enter Second Number:";
+    	    cin>>b;
+    	cout<<"Enter Operetor (+,-,*,/):";
+    	    cin>>op;
+    	    
+    	       switch (op){
+    	       	case '+'
+    	       	cout<<"Addition:"<<a+b;
+    	       	break;
+    	       	
+    	       	case '-'
+    	       	cout<<"Subtraction:"<<a-b;
+    	       	break;
+    	       	
+    	       	case '*'
+    	       	cout<<"Multiplication:"<<a*b;
+    	       	break;
+    	       	
+    	      	case '/':
+    	      	     if (b!=0) {
+    	       	cout<<"Division:"<<a/b;
+    	      	     }else{
+    	      	     		
+    	       	cout<<"Invalid Division!";
+    	      	     }
+    	        	break;
+    	        	
+    	        	default:
+    	        	cout<<"Error!Invalid Opretor";
+    	        	
+    	       }
+    	       
+    }
